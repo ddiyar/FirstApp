e2e37bc27c71a7eb60818db4dd2ef4beb57db5f4
DDiyar <ddogru10@gmail.com>
2021-08-23 13:06:17 +0300

    random dunno

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
