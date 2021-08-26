22c8fc81b715abd74d5562954960b45b9f6cd3cd
DDiyar <ddogru10@gmail.com>
2021-08-26 13:59:20 +0300

    http-a

--- components/List.js
+++ components/List.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {FlatList} from 'react-native';
 import ListItem from './ListItem';
 
-const url = 'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
-let mediaArray = [];
-const loadMedia = async () => {
-  const response = await fetch(url);
-  const json = await response.json();
-  console.log(json);
-};
-
-loadMedia();
-
 const List = (props) => {
+  const [mediaArray, setMediaArray] = useState([]);
+  const url =
+    'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
+
+  useEffect(() => {
+    const loadMedia = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setMediaArray(json);
+      } catch (e) {
+        console.log(e.message());
+      }
+    };
+    loadMedia();
+  }, []);
+
+  console.log('List rivi 15', mediaArray);
   return (
     <FlatList
       data={mediaArray}
       renderItem={({item}) => <ListItem singleMedia={item} />}
+      keyExtractor={(item, index) => index.toString()}
     />
   );
 };
-
-List.propTypes = {};
 
 export default List;
